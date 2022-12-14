4277d2a9d9d4c50f5ce4ca5544ee25c53253eb4e
CelestinaT”git config --global user.name CelestinaT” <celestina.temilade@outlook.comgit config --global user.email celestina.temilade@outlook.com>
2022-12-13 17:41:39 -0800

    Functions_nested_loops_mandatory

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- *  print_last_digit - prints the last digit of a number
- *  @n: the int to extract the last digit from
- *  Return: value of the last digit
+ * print_last_digit - prints the last digit of a number
+ * @n: the int to extract the last digit from
+ * Return: value of the last digit
  */
-int print_last_digit(int)
+int print_last_digit(int);
 {
 	int a;
 
 	if (n < 0)
-        	n = -n;
+		n = -n;
 	a = n % 10;
 	if (a < 0)
 		a = -a;
