95bd53aeb14fd3bca81a90b31b95f8db55b535b2
CelestinaT”git config --global user.name CelestinaT” <celestina.temilade@outlook.comgit config --global user.email celestina.temilade@outlook.com>
2022-12-13 17:59:00 -0800

    Functions_nested_loops_mandatory

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,11 +16,11 @@ void jack_bauer(void)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
-         		_putchar(':');
+			_putchar(':');
 			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0);
+			_putchar((j % 10) + '0');
 			_putchar('\n');
-			j++
+			j++;
 		}
 		i++;
 	}
